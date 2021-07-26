106124a1b3cdb87fb27798c3b14baa71be403ba9
Sreejan <sreejan147@gmail.com>
2021-07-27 00:40:08 +0530

    fixed bugs in navbar and modified fonts in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,26 +15,28 @@ const Navbar = ({ setQuery }) => {
   }
 
   return (
-    <nav className="navbar">
-      <div className="navbar-brand" onClick={() => window.location.reload()}>
-        <img src="./unsplash.png" alt="" className="nav-img" />
-        <div className="brand-name">Pixdeck</div>
-      </div>
-      <div className="nav-search-wrapper">
-        <i className="eos-icons nav-search-icon">search</i>
-        <input
-          type="text"
-          placeholder="Search by name"
-          className="nav-search-bar"
-          // value={query}
-          onChange={(e) => debounceSearch(e)}
-        />
-      </div>
-      {/* <div className="nav-last-div">
+    <div className="navbar-wrapper">
+      <nav className="navbar">
+        <div className="navbar-brand" onClick={() => window.location.reload()}>
+          <img src="./unsplash.png" alt="" className="nav-img" />
+          <div className="brand-name">Pixdeck</div>
+        </div>
+        <div className="nav-search-wrapper">
+          <i className="eos-icons nav-search-icon">search</i>
+          <input
+            type="text"
+            placeholder="Search by name"
+            className="nav-search-bar"
+            // value={query}
+            onChange={(e) => debounceSearch(e)}
+          />
+        </div>
+        {/* <div className="nav-last-div">
         <button className="add-photo-btn">Add a photo</button>
         <i className="eos-icons toggle-nav-icon">menu</i>
       </div> */}
-    </nav>
+      </nav>
+    </div>
   );
 };
 
